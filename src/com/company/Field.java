5f19541e1499371b56f007c79b501ab8098df52d
Arcfej <mayer.miklos91@gmail.com>
2020-02-22 15:55:58 +0000

    Create new classes and interfaces

---
+++ src/com/company/Field.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Field {
+
+    public final boolean up;
+    public final boolean down;
+    public final boolean right;
+    public final boolean left;
+
+    public Field(boolean up, boolean down, boolean right, boolean left) {
+        this.up = up;
+        this.down = down;
+        this.right = right;
+        this.left = left;
+    }
+
+
+}
