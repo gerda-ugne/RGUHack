f575afcda49f06e5fa3106946a4f510234e83bc4
Arcfej <mayer.miklos91@gmail.com>
2020-04-24 23:12:32 +0100

    Set full screen mode on desktop

--- desktop/src/gerda/arcfej/dreamrealm/desktop/DesktopLauncher.java
+++ desktop/src/gerda/arcfej/dreamrealm/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("Dream Realm");
-		config.setWindowedMode(800, 480);
+		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 
 		new Lwjgl3Application(new GameCore(), config);
 	}
